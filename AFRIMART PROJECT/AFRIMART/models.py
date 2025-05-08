aa86855193c92a264338bee7b2426e10d331a8cf
aanneeek <hubgenus1@gmail.com>
2025-05-08 03:24:11 +0100

    i tried to push from my local machine but instead ended up pulling so i started over

--- AFRIMART PROJECT/AFRIMART/models.py
+++ AFRIMART PROJECT/AFRIMART/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    Phone= models.IntegerField()
+    
+class Shop(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="myshops")
+    Photo=models.FileField(upload_to='shop_owner_photos')
+    Id_card =models.FileField(upload_to='shop_owner_IDs')
+    Shop_proof = models.FileField(upload_to='shop_owner_proofs')
 # Create your models here.
--- AFRIMART PROJECT/AFRIMART/urls.py
+++ AFRIMART PROJECT/AFRIMART/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.Afrimarthome)
+    path("",views.Afrimarthome, name="home"),
+    path("registration/",views.Afrimartaccountcreation, name="registration"),
+    path("login/",views.Afrimartlogin, name="login"),
+    path("createshop/", views.Afrimartcreateshop, name="createshop")
+    
 ]
