a98b7e937f535616fb7c49c02ef2b2b608c994de
Onye vik <114454501+aaneeeek@users.noreply.github.com>
2025-05-09 04:17:02 +0000

    all

--- afrismart_project/AfrimartCore/wsgi.py
+++ afrismart_project/AfrimartCore/wsgi.py
@@ -7,12 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "afrismart_project.AfrimartCore.settings")
+
+# application = get_wsgi_application()
+
+# app=application
+
+
 import os
+import sys
+
+# Add the root project directory to sys.path so Python can find AFRIMART app
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "afrismart_project.AfrimartCore.settings")
+# Point to your settings module (make sure it's correct)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AfrimartCore.settings')
 
 application = get_wsgi_application()
-
-app=application
