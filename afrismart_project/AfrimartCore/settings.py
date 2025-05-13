13cfe1d1b96f3238b7438008cfebf77d4aee8573
Onye vik <114454501+aaneeeek@users.noreply.github.com>
2025-05-13 17:34:53 +0000

    host

--- afrismart_project/AfrimartCore/settings.py
+++ afrismart_project/AfrimartCore/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = "static/"
 STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
 
 # MEDIA_ROOT=os.path.join(BASE_DIR,"media")
-MEDIA_ROOT=os.path.join(BASE_DIR,"mediafiles")
+# MEDIA_ROOT=os.path.join(BASE_DIR,"mediafiles")
 
 MEDIA_URL="/Afrimart_files/"
 
