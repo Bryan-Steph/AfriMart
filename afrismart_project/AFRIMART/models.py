59a09c2aaa47cdcc524206cba27c296e6362bcdf
aanneeek <hubgenus1@gmail.com>
2025-06-11 13:34:58 +0100

    last

--- afrismart_project/AFRIMART/models.py
+++ afrismart_project/AFRIMART/models.py
@@ -4,18 +4,27 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     Phone= models.IntegerField()
-    
-class Vendor(models.Model):
-    user=models.OneToOneField(User, on_delete=models.CASCADE, related_name="my_vendor_profile")
-    
+
+
 class Shop(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="myshops")
     Photo=models.FileField(upload_to='shop_owner_photos')
     Id_card =models.FileField(upload_to='shop_owner_IDs')
     Shop_proof = models.FileField(upload_to='shop_owner_proofs')
-    
     def approve(self):#approving virtual shop creation
         Vendor.objects.create(
             user=self.user
         )
+
+class Vendor(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE, related_name="my_vendor_profile")
+    Fullname=models.CharField(max_length=50)
+    Whatsapp_no=models.IntegerField()
+    Additional_phone=models.IntegerField()
+    Shop_name=models.CharField(max_length=50)
+    Market_location=models.CharField(max_length=50)
+    Stall_number=models.CharField(max_length=50, blank=True, null=True)
+    Shop_description=models.TextField()
+    Profile_picture=models.ImageField()
+
 # Create your models here.
--- afrismart_project/AFRIMART/views.py
+++ afrismart_project/AFRIMART/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse, reverse
 from django.contrib.auth.models import User
-from .models import Profile, Shop
+from .models import Profile, Shop, Vendor
 from django.utils.timezone import now
 from django.contrib.auth import login, logout, authenticate
 from .functions import generate_email_comfirmation_code
@@ -193,3 +193,21 @@ def Afrimartapprovedvendor(request):
 
 def vendordashboard(request):
     return render(request, "vendorDashboard.html")
+
+def PUvendorinfo(request, id=1):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    else:
+        try:
+            vendor = Vendor.objects.get(id=id)
+            if vendor.user!=request.user:
+                return HttpResponse('You are not authorised to be on this page')
+            else:
+                if request.method=='POST':
+                    Vendor.objects.create(
+
+                    )
+                return render(request, "PUvendorInfo.html")
+        except:
+            return HttpResponse('This page does not exist')
+    # return render(request, "PUvendorInfo.html")
