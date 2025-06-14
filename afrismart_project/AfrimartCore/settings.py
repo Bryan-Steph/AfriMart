2481aab11d2a55d48225a0d9f20c9e184f021ffe
aanneeek <hubgenus1@gmail.com>
2025-06-14 02:37:51 +0100

    all

--- afrismart_project/AfrimartCore/settings.py
+++ afrismart_project/AfrimartCore/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-h9%^#vsh!)cvu%-h@v@uh+s%n5=15j_elz_2bhj%mk3pih(*d8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'localhost','127.0.0.1']
 
